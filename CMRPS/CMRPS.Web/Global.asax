49ce2d13e07ba1e1c92abcd2778b89e110f84f9d
Niels Schmidt <it.niels@gmail.com>
2016-10-17 15:23:52 +0200

    Added fresh project + Updated NuGet + CSS Theme

---
+++ CMRPS/CMRPS.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CMRPS.Web.MvcApplication" Language="C#" %>
---
+++ CMRPS/CMRPS.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CMRPS.Web.Startup))]
+namespace CMRPS.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
