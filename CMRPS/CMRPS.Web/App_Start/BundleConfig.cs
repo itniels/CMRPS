dc7ba41e0ef68aa0428cd9be9bfc48e6a8f5a254
Niels Schmidt <it.niels@gmail.com>
2016-10-27 10:15:30 +0200

    Added site.js file and started work on SignalR

--- CMRPS/CMRPS.Web/App_Start/BundleConfig.cs
+++ CMRPS/CMRPS.Web/App_Start/BundleConfig.cs
@@ -26,6 +26,9 @@ namespace CMRPS.Web
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/Bundles/Site").Include(
+                "~/Scripts/Site.js"));
         }
     }
 }
---
+++ CMRPS/CMRPS.Web/Hubs/Connected.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace CMRPS.Web.Hubs
+{
+    [HubName("ConnectedHub")]
+    public class Connected : Hub
+    {
+        public void isConnected()
+        {
+            Clients.All.hello();
+        }
+    }
+}
